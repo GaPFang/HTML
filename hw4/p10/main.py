bfaea2d18f7a190527cfa815fc37db76dffe9d6d
jialin <jlfang0119@gmail.com>
2023-11-11 22:55:23 +0800

    hw4/p10

--- hw4/p10/main.py
+++ hw4/p10/main.py
@@ -1,44 +1,43 @@
 from liblinear.liblinearutil import *
 import numpy as np
 
-train = []
+train_data = []
 x = []
 y = []
 z_dict = []
 z_list = []
 _lambda = [1e-6, 1e-4, 1e-2, 1, 1e2]
 
+train_input = open("train.txt", "r")
 for i in range(200):
-    train.append(input('').split())
-    x.append(train[i][1:7])
-    y.append(int(train[i][6]))
-    train[i][1:7] = train[i][0:6]
-    train[i][0] = 1
+    train_data.append(train_input.readline().split())
+    x.append(train_data[i][1:7])
+    y.append(int(train_data[i][6]))
+    train_data[i][1:7] = train_data[i][0:6]
+    train_data[i][0] = 1
     for j in range(1, 7):
-        x.append(float(train[i][j]))
+        x.append(float(train_data[i][j]))
     z_dict.append({})
     z_list.append([])
     j = 1
     for a in range(7):
         for b in range(a, 7):
             for c in range(b, 7):
-                tmp = float(train[i][a]) * float(train[i][b]) * float(train[i][c])
+                tmp = float(train_data[i][a]) * float(train_data[i][b]) * float(train_data[i][c])
                 z_dict[i].update({j:tmp})
                 z_list[i].append(tmp)
                 j += 1
 
 prob = problem(y, z_dict)
 
+best_E_in = 1
+
+f = open("E_in.txt", "w")
 for i in range(len(_lambda)):
     _C = 1 / (2 * _lambda[i])
     param = parameter('-s 0 -c ' + str(_C) + ' -e 0.000001')
     m = train(prob, param)
     save_model("w_log_lambda=" + str(_lambda[i]) + ".model", m)
-
-best_E_in = 1
-
-f = open("E_in.txt", "w")
-for i in range(len(_lambda)):
     f1 = open("w_log_lambda=" + str(_lambda[i]) + ".model", "r")
     for j in range(6):
         f1.readline()

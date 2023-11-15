6e6eb331ac5fce157c49eaa6134b446295ae5cce
jialin <jlfang0119@gmail.com>
2023-11-15 14:54:37 +0800

    cross validation

--- hw4/p12/main.py
+++ hw4/p12/main.py
@@ -36,14 +36,33 @@ best_lambda = np.zeros(repeat)
 
 f = open("E_in.txt", "w")
 for r in range(repeat):
-    random.seed(r)
-    E_CV = []
+    np.random.seed(r)
+    mess = np.random.permutation(N).tolist()
+    fold = [mess[:int(N/5)]] + [mess[int(N/5):int(2 * N/5)]] + [mess[int(2 * N/5):int(3 * N/5)]] + [mess[int(3 * N/5):int(4 * N/5)]] + [mess[int(4 * N/5):N]]
+    E_CV = np.zeros(5).tolist()
     for i in range(len(_lambda)):
         _C = 1 / (2 * _lambda[i])
-        param = parameter('-s 0 -c ' + str(_C) + ' -e 0.000001 -v 5')
-        prob = problem(y_all, z_all)
-        CV_ACC = train(prob, param)
-        E_CV.append((1 - CV_ACC) / 100)
+        param = parameter('-s 0 -c ' + str(_C) + ' -e 0.000001')
+        for v in range(5):
+            y_train = []
+            z_train = []
+            y_val = []
+            z_val = []
+            for j in range(5):
+                if j == v:
+                    for k in range(int(N/5)):
+                        y_val.append(y_all[fold[j][k]])
+                        z_val.append(z_all[fold[j][k]])
+                else:
+                    for k in range(int(N/5)):
+                        y_train.append(y_all[fold[j][k]])
+                        z_train.append(z_all[fold[j][k]])
+            print(z_train)
+            prob = problem(y_train, z_train)
+            m = train(prob, param)
+            p_label, p_acc, p_val = predict(y_val, z_val, m)
+            E_CV[i] += ((1 - p_acc[0]) / 100)
+        E_CV[i] /= 5
         if (E_CV[i] <= best_E_CV[r]):
             best_E_CV[r] = E_CV[i]
             best_lambda[r] = _lambda[i]
@@ -53,4 +72,6 @@ f.close()
 bins = np.linspace(-6.5, 2.5, 10)
 plt.hist(np.log10(best_lambda), bins, label='log_10(lambda*)')
 plt.title('P12: Histogram of log_10(lambda*)')
+plt.xlabel('log_10(lambda*)')
+plt.ylabel('#')
 plt.show()

44e5ad8a2490debe138aa23de73d48123df01132
jialin <jlfang0119@gmail.com>
2023-12-03 15:46:49 +0800

    hw5/p11

--- hw5/p11/main.py
+++ hw5/p11/main.py
@@ -8,12 +8,11 @@ import matplotlib.pyplot as plt
 C = [0.01, 0.1, 1, 10, 100]
 repeat = 100
 
-Eout_fd = open("Eout.txt", "w")
 y_all, x_all = svm_read_problem('../train.txt')
 array = [i for i in range(len(y_all))]
 
 for i in range(len(y_all)):
-    if y_all[i] == 1:
+    if y_all[i] == 3:
         y_all[i] = 1
     else:
         y_all[i] = -1
@@ -43,15 +42,20 @@ for r in range(repeat):
         train_prob = svm_problem(y_train, x_train)
         m = svm_train(train_prob, param)
         p_labs, p_acc, p_vals = svm_predict(y_val, x_val, m)
-        Eval = (100 - p_acc[0]) / 100
+        Eval = 0
+        for j in range(len(p_labs)):
+            if (p_labs[j] * y_val[j] < 0):
+                Eval += 1
+        Eval /= len(p_labs)
         if Eval < min_Eval:
             min_Eval = Eval
             min_C = c
     min_C_arr[str(min_C)] += 1
 for c in C:
     min_C_arr[str(c)] /= repeat
+
+print("C_frequency = " + str(min_C_arr))
 plt.bar(list(min_C_arr.keys()), list(min_C_arr.values()))
- 
 plt.xlabel("C*")
 plt.ylabel("count")
 plt.title("bar chart of C versus its selection frequency")
